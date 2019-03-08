d9cf438d2bc1424860f8fde6b955ca1c6bdcede8
Root USO User <student@stud.acs.upb.ro>
2019-03-08 09:57:08 +0200

    implemented taks for laboratory 03

---
+++ app/src/main/java/ro/pub/cs/systems/eim/lab03/phonedialer/PhoneDialerActivity.java
@@ -0,0 +1,29 @@
+package ro.pub.cs.systems.eim.lab03.phonedialer;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.GridLayout;
+
+public class PhoneDialerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phone_dialer);
+        final GridLayout grid = findViewById(R.id.gridlayout);
+        int i
+        for()grid.getChildCount()
+
+        /*button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+            }
+        });*/
+    }
+    public void onClick(View view) {
+
+    }
+}
